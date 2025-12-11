4d2c18e1197a0fc322f015d767f317bc31063fab
once upon the bed <3865245887@qq.com>
2025-12-11 13:58:23 +0800

    Update bubble sort.c

--- bubble sort.c
+++ bubble sort.c
@@ -26,12 +26,12 @@ int main()
 }
 int bubble_sort(int array[],int n){
     int move=0;
-    for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            if(array[i]>array[j]){
-                int temp=array[i];
-                array[i]=array[j];
-                array[j]=temp;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(array[j]>array[j+1]){
+                int temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
                 move++;
             }
         }
